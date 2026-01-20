8d520ae3c86cc8d80a6612d38609dc4d3feac3cf
thespeeder5.0.9 <thespeeder5.0.9@gmail.com>
2026-01-20 18:37:13 +0000

    feat: add generated Android project files

---
+++ app/src/main/java/com/kryptopass/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.kryptopass.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
